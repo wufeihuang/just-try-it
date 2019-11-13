73a29e5ee44699b7371c8874ac76a1900a0a3195
wufeihuang <1710333107@qq.com>
2019-11-13 23:33:13 +0800

    11.13

--- src/page-home/index.js
+++ src/page-home/index.js
@@ -1,5 +1,107 @@
 import React from 'react'
 
+class Child extends React.Component {
+  componentWillMount() {
+    console.log('Child: componentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('Child: componentDidMount')
+  }
+
+  componentWillUnmount(){
+    console.log('Child: componentWillUnmount')
+  }
+
+  componentWillUpdate() {
+    console.log('Child: componentWillUpdate')
+  }
+
+  componentDidUpdate() {
+    console.log('Child: componentDidUpdate')
+  }
+
+  // getDerivedStateFromProps() {
+
+  // }
+
+  // getSnapshotBeforeUpdate() {
+  //   console.log('Child: getSnapshotBeforeUpdate')
+  // }
+
+  shouldComponentUpdate() {
+    console.log('Child: shouldComponentUpdate')
+    return true
+  }
+
+  render() {
+    return <div>Child</div>
+  }
+}
+
+class Parent extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+    }
+  
+    this.ref = React.createRef()
+  }
+  
+
+  componentWillMount() {
+    console.log('Parent: componentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('Parent: componentDidMount')
+
+    this.ref.current.addEventListener('click', () => {
+      console.log('before setState: ', this.state.count)
+      // this.setState({
+      //   count: this.state.count + 1
+      // })
+      this.setState((state, props) => ({
+        count: state.count + 1,
+      }))
+      console.log('after setState: ', this.state.count)
+    })
+  }
+
+  componentWillUnmount(){
+    console.log('Parent: componentWillUnmount')
+  }
+
+  componentWillUpdate() {
+    console.log('Parent: componentWillUpdate')
+  }
+
+  componentDidUpdate() {
+    console.log('Parent: componentDidUpdate')
+  }
+
+  shouldComponentUpdate() {
+    console.log('Parent: shouldComponentUpdate')
+    return true
+  }
+
+  // getDerivedStateFromProps() {
+
+  // }
+
+  // getSnapshotBeforeUpdate() {
+  //   console.log('Parent: getSnapshotBeforeUpdate')
+  // }
+
+  render() {
+    return <div>
+      <h2 ref={this.ref}>Parent, click</h2>
+      <Child />
+    </div>
+  }
+}
+
 const App = () => {
   const [temp, setTemp] = React.useState(5);
 
@@ -30,6 +132,7 @@ export default function() {
     <div className="home--content">
       content
       <App />
+      <Parent />
     </div>
   )
 }
